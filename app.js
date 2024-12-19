bbbe32aa329f21b59b20dc6789539a74c7a96ce7
huseyn1211 <quluzadeh43@gmail.com>
2024-12-19 13:26:35 +0400

    home

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = document.createElement("li")
+let b = document.querySelector("ul")
+
+ b.append(a)
+ function myFunc(newName){
+    a.innerText =newName
+
+ }
+//  myFunc("selim memmedov")
+  myFunc("aga agazade")
+
+// myFunc("memmed memedov")
